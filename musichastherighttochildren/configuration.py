2f546cbcb5517827133191bee7d0fd40c78050b3
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2012-06-10 17:15:40 +0200

    Creating SimpleDB if it doesn’t exist yet.

--- musichastherighttochildren/configuration.py
+++ musichastherighttochildren/configuration.py
@@ -3,5 +3,5 @@
 class Configuration():
 	COLLECTION		  = '/Users/michiel/Music/iTunes/iTunes Music/Music'
 	HAS_LASTFM		  = True
-	AWS_ACCESS_KEY	  = ''
-	AWS_SECRET_KEY	  = ''
+	AWS_ACCESS_KEY	  = 'AKIAIG6QTY3GEPNCTAXQ'
+	AWS_SECRET_KEY	  = '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'
--- musichastherighttochildren/simpledb.py
+++ musichastherighttochildren/simpledb.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 import boto
+from boto.exception import SDBResponseError
+
 
 class SimpleDB:
     SDB = None
@@ -8,7 +10,11 @@ class SimpleDB:
 
     def __init__(self, awsaccesskey, awssecretkey, domainname):
         self.SDB = boto.connect_sdb(awsaccesskey, awssecretkey)
-        self.DOMAIN = self.SDB.get_domain(domainname)
+        try:
+            self.DOMAIN = self.SDB.get_domain(domainname)
+        except SDBResponseError, e:
+            print u'SimpleDB domain doesn’t exist yet, creating domain with name %s' % domainname
+            self.DOMAIN = self.SDB.create_domain(domainname)
 
     def writeItem(self, **attributes):
         item = self.DOMAIN.new_item('item')
