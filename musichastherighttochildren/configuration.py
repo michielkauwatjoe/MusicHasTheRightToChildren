2d1bd5835e0502ad71ab0857d48f08d062647882
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2012-06-10 17:18:55 +0200

    Ignoring, should contain access keys and user specific information.

--- musichastherighttochildren/configuration.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-class Configuration():
-	COLLECTION		  = '/Users/<username>/Music/iTunes/iTunes Music/Music'
-	HAS_LASTFM		  = True
-	LASTFM_USERNAME   = 'lastfm-username'
-	AWS_ACCESS_KEY	  = ''
-	AWS_SECRET_KEY	  = ''
