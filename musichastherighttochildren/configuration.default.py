e32d95cd0929571081973957fdf120df056c804f
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2013-07-15 16:13:45 +0200

    Removing old key from default configuration.

--- musichastherighttochildren/configuration.default.py
+++ musichastherighttochildren/configuration.default.py
@@ -6,5 +6,5 @@ class Configuration():
 	"""
 	COLLECTION		  = '/Users/michiel/Music/iTunes/iTunes Music/Music'
 	HAS_LASTFM		  = True
-	AWS_ACCESS_KEY	  = 'AKIAIG6QTY3GEPNCTAXQ'
-	AWS_SECRET_KEY	  = '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'
+	AWS_ACCESS_KEY	  = ''
+	AWS_SECRET_KEY	  = ''
