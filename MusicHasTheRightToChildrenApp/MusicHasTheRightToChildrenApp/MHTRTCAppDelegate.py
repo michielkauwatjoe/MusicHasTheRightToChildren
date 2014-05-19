2e22ac1d52ece7cc2bd054f394fff1adccdc6235
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-05-19 16:44:49 +0200

    Adding target.

---
+++ MusicHasTheRightToChildrenApp/MusicHasTheRightToChildrenApp/MHTRTCAppDelegate.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+#
+#  MusicHasTheRightToChildrenAppAppDelegate.py
+#  MusicHasTheRightToChildrenApp
+#
+#  Created by Michiel on 19/05/14.
+#  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+#
+
+from Foundation import *
+from AppKit import *
+
+class MHTRTCAppDelegate(NSObject):
+    def applicationDidFinishLaunching_(self, sender):
+        NSLog("Application did finish launching.")
---
+++ MusicHasTheRightToChildrenApp/MusicHasTheRightToChildrenApp/main.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+#
+#  main.py
+#  MusicHasTheRightToChildrenApp
+#
+#  Created by Michiel on 19/05/14.
+#  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+#
+
+#import modules required by application
+import objc
+import Foundation
+import AppKit
+
+from PyObjCTools import AppHelper
+
+# import modules containing classes required to start application and load MainMenu.nib
+import MHTRTCAppDelegate
+
+# pass control to AppKit
+AppHelper.runEventLoop()
