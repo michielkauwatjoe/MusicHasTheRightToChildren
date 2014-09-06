a8e44963fdd0d7aa8c60771cff0f86d12de1494c
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-09-06 10:42:27 +0200

    Checks if backup album path exists.

--- musichastherighttochildren/scanners/folderscanner.py
+++ musichastherighttochildren/scanners/folderscanner.py
@@ -19,14 +19,30 @@ class FolderScanner(MHTRTCGlobals):
 
         for artist, albums, _ in os.walk(path):
             if len(albums) > 0:
-                print self.stripArtist(artist), albums
+                collection[self.stripArtist(artist)] = albums
 
+        return collection
 
     def stripArtist(self, artist):
         return artist.replace(self.COLLECTION, '')
 
+    def backupPath(self, artist, album):
+        return self.BACKUP + artist + '/' + album
+
+
+    def backedUp(self, path):
+        if os.path.exists(path):
+            return True
+        return False
+
     def main(self):
         itunes = self.walk(self.COLLECTION)
+
+        for artist, albums in itunes.items():
+            for album in albums:
+                path = self.backupPath(artist, album)
+                if not self.backedUp(path):
+                    print path
 
 if __name__ == '__main__':
     scanner = FolderScanner()
