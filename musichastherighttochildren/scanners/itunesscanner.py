56c2addf5749bc9e8ae437a1cab3d39733817906
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-09-10 15:42:16 +0200

    Converting XML values to dictionary.

--- musichastherighttochildren/scanners/itunesscanner.py
+++ musichastherighttochildren/scanners/itunesscanner.py
@@ -15,18 +15,100 @@ class iTunesScanner(MHTRTCGlobals):
 
     def scan(self, library_file=None):
         u"""
+        Loads iTunes library.
+        Database keys:
+
+        "Library Persistent ID",
+        Tracks,
+        "Minor Version",
+        Date,
+        Features,
+        Playlists,
+        "Major Version",
+        "Show Content Ratings",
+        "Application Version",
+        "Music Folder"
         """
         libfile = library_file or self.LIBRARY
+        self.db = Foundation.NSDictionary.dictionaryWithContentsOfFile_(libfile)
+        self.tracks = self.db['Tracks'] # objective-c class __NSCFDictionary
+        self.titleindex = {}
+        self.buildTitleIndex()
+        self.checkFilesExist()
 
-        # Load iTunes library
-        db = Foundation.NSDictionary.dictionaryWithContentsOfFile_(libfile)
+    def buildTitleIndex(self):
+        """
 
-        self.checkFilesExist(db)
+        Example track values:
 
-    def checkFilesExist(self, db):
+        Album = "Footwork EP";
+        "Album Artist" = "Jamie Grind";
+        Artist = "Jamie Grind";
+        "Artwork Count" = 1;
+        "Bit Rate" = 320;
+        "Date Added" = "2014-05-16 12:14:37 +0000";
+        "Date Modified" = "2011-01-17 13:55:31 +0000";
+        "Disc Count" = 1;
+        "Disc Number" = 1;
+        "File Folder Count" = 5;
+        Genre = Electronic;
+        Kind = "MPEG audio file";
+        "Library Folder Count" = 1;
+        Location = "file://localhost/Users/michiel/Music/iTunes/iTunes%20Media/Music/Jamie%20Grind/Footwork%20EP/01%20Footwork.mp3";
+        Name = Footwork;
+        "Persistent ID" = 5FE4748CF01018FD;
+        "Sample Rate" = 44100;
+        Size = 12740768;
+        "Total Time" = 314148;
+        "Track Count" = 4;
+        "Track ID" = 2974;
+        "Track Number" = 1;
+        "Track Type" = File;
+        Year = 2011;
+        """
+        for track in self.tracks.itervalues():
+            tid = track['Track ID']
+
+            if 'Album Artist' in track:
+                artist = track['Album Artist']
+            elif 'Artist' in track:
+                artist = track['Artist']
+            else:
+                print 'Unknown artist for track ID %d' % tid
+                continue
+
+            album = track['Album']
+            title = track['Name']
+
+            if 'Track Number' in track:
+                nr = track['Track Number']
+            else:
+                print 'Unknown number for track %d (ID), %s' % (tid, title)
+                continue
+
+            if 'Disc Number' in track:
+                disc = track['Disc Number']
+            else:
+                print 'Unknown number for track %d (ID), %s' % (tid, title)
+                disc = 1
+                print 'Assuming disc number is %d' % disc
+
+            if artist not in self.titleindex:
+                self.titleindex[artist] = {}
+
+            if album not in self.titleindex[artist]:
+                self.titleindex[artist][album] = {}
+
+            if disc not in self.titleindex[artist][album]:
+                self.titleindex[artist][album][disc] = {}
+
+            self.titleindex[artist][album][disc][nr] = {'title': title, 'tid': tid}
+
+        print self.titleindex.items()
+
+    def checkFilesExist(self):
         # Check track info.
-        for track in db[u'Tracks'].itervalues():
-            print track
+        for track in self.tracks.itervalues():
 
             if u'Location' not in track:
                 print 'No location info in track info', track
