bfa0d448f9b37dea8e5e02106c3ea1c5b7963ac3
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-09-10 10:12:45 +0200

    Added function to load iTunes library XML.

--- musichastherighttochildren/scanners/itunesscanner.py
+++ musichastherighttochildren/scanners/itunesscanner.py
@@ -2,6 +2,39 @@
 # -*- coding: utf-8 -*-
 #
 # https://github.com/michielkauwatjoe/MusicHasTheRightToChildren
+import sys
+import os
+import Foundation
 
 class iTunesScanner(object):
-    pass
+    u"""
+    PyObjc solution for access to the iTunes library.
+    """
+
+    def scan_itunes_library(library_file):
+        u"""
+        """
+
+        # Load iTunes library
+        db = Foundation.NSDictionary.dictionaryWithContentsOfFile_(library_file)
+
+        # Check track info.
+        for track in db[u'Tracks'].itervalues():
+
+            if u'Location' not in track:
+                print 'No location info in track info', track
+                continue
+
+            # Resolve location
+            nsurl = Foundation.NSURL.URLWithString_(track[u'Location'])
+
+            # Check local file paths
+            if nsurl.scheme() == u'file' and nsurl.host() == u'localhost':
+                if not os.path.exists(nsurl.path()):
+                    print "Location does not exist:", nsurl.path(), 'from track', track
+            else:
+                print "Don't know how to check", nsurl
+
+if __name__ == '__main__':
+    is = iTunesScanner()
+    is.scan_itunes_library(library_file=sys.argv[1])
