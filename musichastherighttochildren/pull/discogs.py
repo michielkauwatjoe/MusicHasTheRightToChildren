15536221f97a6a6509574c976300aa8257589add
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-10-14 20:33:38 +0200

    Moving CSV parsing to Discogs.

--- musichastherighttochildren/pull/discogs.py
+++ musichastherighttochildren/pull/discogs.py
@@ -5,6 +5,8 @@
 
 import oauth2 as oauth
 import urlparse
+import csv
+import json
 
 from musichastherighttochildren.aux.shell import Shell
 from musichastherighttochildren.mhtrtcglobals import MHTRTCGlobals
@@ -16,4 +18,23 @@ class Discogs(MHTRTCGlobals):
     """
 
     def __init__(self):
-        pass
+        discogs = self.readCSV()
+        discogs_json = json.dumps(discogs)
+        print discogs_json
+
+
+    def readCSV(self):
+        discogs = {}
+        with open('../data/al-khwarizmi-collection-20141013-1124.csv', 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                artist = row[1]
+                album = '%s (%s)' % (row[2], row[4])
+
+                if not artist in discogs:
+                    discogs[artist] = []
+                discogs[artist].append(album)
+        return discogs
+
+if __name__ == '__main__':
+    discogs = Discogs()
