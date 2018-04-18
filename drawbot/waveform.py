388ccabb1a0282fae22e3e6b083c6c9ae689ec67
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-04-18 11:07:23 +0200

    Plotting example waveform.

--- drawbot/waveform.py
+++ drawbot/waveform.py
@@ -1,10 +1,65 @@
-path = '/Users/michiel/Downloads/human-zoo-3-side-a.wav'
+path = '/Users/michiel/Downloads/test.wav'
 
 from scipy.io.wavfile import read
-#import matplotlib.pyplot as plt
 
+samples = 500000
+w = width()
+h = height()
+maxy = h / 2
+dy = h / 4
 # read audio samples
 input_data = read(path)
 #print(input_data)
-audio = input_data[1]
-print(audio[0:12])
+rawaudio = input_data[1]
+
+l = len(rawaudio)
+print('Total number of samples: %d' % l)
+
+if samples < l:
+    audio = rawaudio[0:samples+1]
+else:
+    print('Full length')
+    audio= rawaudio[0:]
+    samples = l
+    
+
+
+low = audio[:,0]
+high = audio[:,1]
+lowest = int(min(low))
+highest = int(max(high))
+diff = highest - lowest
+yscale = maxy / diff
+meany = h / 2
+
+#print('%s %s' % (lowest,  highest))
+
+stroke(0.8, 0.8, 0)
+strokeWidth(1)
+interval = 0
+
+di = 50 # Drop samples at higher zoom level.
+xscale = w / samples * di
+
+for x in range(0, l):
+    if interval > l:
+        break
+    sample = audio[interval]
+    y0 = (int(sample[0]) + abs(lowest)) * yscale + dy
+
+    if y0 > meany:
+        y0 = meany
+        
+    y1 = (int(sample[1]) + abs(lowest)) * yscale + dy
+    
+    if y1 < meany:
+        y1 = meany
+        
+    #print('%s, %s' % (x, y0))
+    #print('%s, %s' % (x, y1))
+    newPath()
+    moveTo((x * xscale, y0))
+    lineTo((x * xscale, y1))
+    closePath()
+    drawPath()
+    interval += di
